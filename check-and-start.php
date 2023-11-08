da35a81edc3e27f64bada1090274ecdd46602486
Jason Woitalla <jason.woitalla@gmail.com>
2023-11-08 09:10:12 -0600

    added start command

---
+++ check-and-start.php
@@ -0,0 +1,14 @@
+<?php
+// Define the path to your Next.js app and its start command
+$appPath = "/public_html/todd-co-kennel-react";
+$startCommand = "nohup npm run start";
+
+// Check if the app is running
+exec("pgrep -f \"$startCommand\"", $output, $returnCode);
+
+if ($returnCode !== 0) {
+    // If not running, start the app
+    exec("cd \"$appPath\" && $startCommand > /dev/null 2>&1 &");
+}
+?>
+
